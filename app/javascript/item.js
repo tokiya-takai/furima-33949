29b77bf8c372d5fc5554d78451b923c424b06efe
tokiya-takai <t.orca.dolphin@gmail.com>
2021-01-19 01:48:17 +0900

    販売価格による手数料等の表示

---
+++ app/javascript/item.js
@@ -0,0 +1,17 @@
+function item() {
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  let addTax;
+  let tax = 0.1;
+
+  itemPrice.addEventListener('change', () => {
+    console.log(itemPrice.value);
+    let price = itemPrice.value;
+    addTax = Math.floor(price * tax);
+    addTaxPrice.innerHTML = addTax;
+    profit.innerHTML = Math.floor(price - addTax);
+  });
+}
+window.addEventListener('load', item);
