ed3207b777284f34a8f9e7453b15690321fc72d6
tokiya-takai <t.orca.dolphin@gmail.com>
2021-01-18 22:51:35 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,27 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :delivery
+  belongs_to :place
+  belongs_to :duration
+  
+  #販売価格は300円以上9999999以下であり、半角数値のみであること
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
+  validates :price, numericality: { greater_than_or_equal_to: 300}, allow_blank: true
+  validates :price, numericality: { less_than_or_equal_to: 9999999}, allow_blank: true
+  validates :price, format: { with: VALID_PRICE_REGEX }, allow_blank: true
 
   with_options presence: true do
+    validates :image
     validates :title
     validates :content
     validates :price
   end
+
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
@@ -16,4 +29,6 @@ class Item < ApplicationRecord
     validates :place_id
     validates :duration_id
   end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_084039) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "titile", default: "", null: false
+    t.string "title", default: "", null: false
     t.text "content", null: false
     t.integer "category_id", null: false
     t.integer "status_id", null: false
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 2021_01_18_084039) do
     t.integer "place_id", null: false
     t.integer "duration_id", null: false
     t.integer "price", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_01_18_084039) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
