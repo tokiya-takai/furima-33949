8d4c60b603207ae6ca61b7bc99147216a19d4926
tokiya-takai <t.orca.dolphin@gmail.com>
2021-01-18 16:40:54 +0900

    with_optionsに切り替えと、不要コメントの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,34 +10,33 @@ class User < ApplicationRecord
   JAPANESE_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
   KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
 
-  #ニックネームが必須であること
-  validates :nickname, presence: true
+  with_options presence: true do
+    #ニックネームが必須であること
+    validates :nickname
 
-  #メールアドレスが必須であり、@を含んでおり、重複していないこと
-  validates :email, uniqueness: true
-  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
-  validates :email, presence: true
+    #メールアドレスが必須であり、@を含んでおり、重複していないこと
+    validates :email, uniqueness: true
+    validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
 
-  #パスワードが必須であり、半角英数字混合の6文字以上であること
-  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: {minimum: 6}, allow_blank: true
-  validates :password, confirmation: true
-  validates :password, presence: true
+    #パスワードが必須であり、半角英数字混合の6文字以上であること
+    validates :password, format: { with: VALID_PASSWORD_REGEX }, length: {minimum: 6}, allow_blank: true
+    validates :password, confirmation: true
 
-  #名字と名前が必須であり、全角ひらがな・カタカナ・漢字であること
-  validates :family_name, format: { with: JAPANESE_REGEX} , allow_blank: true
-  validates :family_name, presence: true
+    #名字と名前が必須であり、全角ひらがな・カタカナ・漢字であること
+    validates :family_name, format: { with: JAPANESE_REGEX} , allow_blank: true
+    validates :family_name
 
-  validates :first_name, format: { with: JAPANESE_REGEX }, allow_blank: true
-  validates :first_name, presence: true
+    validates :first_name, format: { with: JAPANESE_REGEX }, allow_blank: true
+    validates :first_name
 
-  #名字と名前のフリガナ必須であり、全角カタカナであること
-  validates :family_name_kana, format: { with: KATAKANA_REGEX }, allow_blank: true
-  validates :family_name_kana, presence: true
+    #名字と名前のフリガナ必須であり、全角カタカナであること
+    validates :family_name_kana, format: { with: KATAKANA_REGEX }, allow_blank: true
+    validates :family_name_kana
 
-  validates :first_name_kana, format: { with: KATAKANA_REGEX }, allow_blank: true
-  validates :first_name_kana, presence: true
+    validates :first_name_kana, format: { with: KATAKANA_REGEX }, allow_blank: true
+    validates :first_name_kana
 
-  #生年月日が必須であること
-  validates :birthday, presence: true
-
+    #生年月日が必須であること
+    validates :birthday
+  end
 end
